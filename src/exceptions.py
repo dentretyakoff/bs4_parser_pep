ee90cbaa0f7c04f9b9f153e60e0a8c39a25a24a8
Denis Tretyakov <den.tretyakoff@yandex.ru>
2023-11-29 18:17:12 +0700

    Исключение для пустого списка от метода find_all.

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,3 +1,9 @@
 class ParserFindTagException(Exception):
     """Вызывается, когда парсер не может найти тег."""
     pass
+
+
+class EmptyListFromFindAll(Exception):
+    """Вызывается при переборе пустого списка,
+    полученного методом BeautifulSoup(text).find_all('tag')"""
+    pass
